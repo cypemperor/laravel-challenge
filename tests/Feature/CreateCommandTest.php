8184d5e2b16232426d62d7e6a54e5dd3c8ed2a99
Timuçin Gündüz <timu911@hotmail.com>
2023-03-28 00:53:32 +0100

    added test for create ticket command

---
+++ tests/Feature/CreateCommandTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class CreateCommandTest extends TestCase
+{
+    /**
+     * Create ticket command test
+     */
+    public function test_createTicket(): void
+    {
+        $this->artisan('create-ticket')
+            ->expectsOutput('Ticket Created.');
+    }
+}
