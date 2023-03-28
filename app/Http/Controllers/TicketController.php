8907a13c903c9532811c89a4a62cf4684e3cce46
Timuçin Gündüz <timu911@hotmail.com>
2023-03-28 02:00:38 +0100

    added controller methods

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
+    public function unprocessedTickets()
+    {
+        $unprocessedTickets = Ticket::where('ticket_status', false)->get();
+
+        return response()->json($unprocessedTickets);
+    }
+
+    public function processedTickets()
+    {
+        $unprocessedTickets = Ticket::where('ticket_status', true)->get();
+
+        return response()->json($unprocessedTickets);
+    }
+
+    public function userTickets(string $email)
+    {
+        $userTickets = Ticket::where('user_email', $email)->get();
+
+        return response()->json($userTickets);
+    }
 }
