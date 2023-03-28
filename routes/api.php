c930815d49a6d3a3a550c6377055168797b7b6a4
Timuçin Gündüz <timu911@hotmail.com>
2023-03-28 01:36:33 +0100

    Added Ticket Controller and API routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TicketController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/tickets/unprocessed', [TicketController::class, 'unprocessedTickets']);
+Route::get('/tickets/processed', [TicketController::class, 'processedTickets']);
+Route::get('/tickets/user/{email}', [TicketController::class, 'userTickets']);
+
