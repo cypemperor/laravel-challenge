c3bbe52897f8ac1e5e6bc32bfe310ed40cc0cd1c
Timuçin Gündüz <timu911@hotmail.com>
2023-03-28 04:15:15 +0100

    Added another test for user tickets endpoint in case the user has no tickets.

---
+++ tests/Feature/APIRoutes/UserHasNoTicketsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature\APIRoutes;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserHasNoTicketsTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_userHasNoTickets(): void
+    {
+        $response = $this->get('/api/tickets/user/nouser@nouserexists.info');
+
+        $response->assertStatus(404);
+        $response->assertJson([
+            'message' => 'There is no ticket created by the email provided.'
+        ]);
+    }
+}
